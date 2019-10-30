1cbc1b916044b9b5f201e83c88c990458bc06c47
SageSanyue <562441461@qq.com>
2019-10-30 22:43:36 +0800

    mutation操作演示

--- src/main.js
+++ src/main.js
@@ -7,6 +7,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
+  store,  // 使用store
   render: h => h(App)
 }).$mount('#app')
--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,20 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count: 1
+  }, // vuex中的数据源，需保存的数据就保存在这里
+  getters: {
+    getNum: function(state) {
+      return state.count + 1
+    }
   },
   mutations: {
+    add(state) {
+      state.count += 1
+    },
+    reduce(state) {
+      state.count -= 1
+    }
   },
   actions: {
   },
